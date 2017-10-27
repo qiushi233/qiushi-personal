27f09f717d44d2d993c5ec594e0c6cf51963bef4
qiushi02 <qiushi02@meituan.com>
2017-10-27 10:40:30 +0800

    first commit

---
+++ src/main/java/com/meituan/spring/AnnotationDemo1.java
@@ -0,0 +1,12 @@
+package com.meituan.spring;
+
+/**
+ * spring注解demo
+ * author: qiushi
+ * date  : 2017/10/27
+ */
+public class AnnotationDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
