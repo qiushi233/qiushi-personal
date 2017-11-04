b2afe14e45ed302cd05b111a70e80581fe3c066c
qiushi02 <qiushi02@meituan.com>
2017-11-04 15:55:43 +0800

    first commit

--- src/main/java/com/meituan/spring/AnnotationDemo1.java
+++ src/main/java/com/meituan/spring/AnnotationDemo1.java
@@ -1,5 +1,8 @@
 package com.meituan.spring;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * spring注解demo
  * author: qiushi
@@ -7,6 +10,14 @@ package com.meituan.spring;
  */
 public class AnnotationDemo1 {
     public static void main(String[] args) {
+        Long now = new Date().getTime();
+        System.out.println(now);
+
+        String res;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String format = sdf.format(now);
+        System.out.println(format);
+
 
     }
 }
